c6141a1e2c5a8d075f1e0d570860623d2864a0cd
Leon <bsexp357079@gmail.com>
2022-01-10 16:54:43 +0800

    Anim & controller update
    1. Add more animations.
    2. Add more control.

--- Project_LinsTale_Normal/Assets/Scripts/playerMovement.cs
+++ Project_LinsTale_Normal/Assets/Scripts/playerMovement.cs
@@ -7,19 +7,49 @@ public class playerMovement : MonoBehaviour
     public characterController2D controller;
     public Animator anim;
 
+    public bool ctrlable = true;
     public float moveSpeed = 40f;
 
     float horizontalMove = 0f;
+    float _moveSpeed;
+    bool jump = false;
 
+    private void Start()
+    {
+        _moveSpeed = moveSpeed;
+        
+    }
     private void Update()
     {
-        //Debug.Log(Input.GetAxisRaw("Horizontal"));
-        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
-        anim.SetFloat("moveSpeed", Mathf.Abs(horizontalMove));
+        if (ctrlable)
+        {
+            //Debug.Log(Input.GetAxisRaw("Horizontal"));
+            
+            horizontalMove = Input.GetAxisRaw("Horizontal") * _moveSpeed;
+            anim.SetFloat("moveSpeed", Mathf.Abs(horizontalMove));
+            if (Input.GetAxisRaw("Vertical") < 0f)
+            {
+                anim.SetBool("isCrouch", true);
+                _moveSpeed = moveSpeed / 2;
+            }
+            else
+            {
+                anim.SetBool("isCrouch", false);
+                _moveSpeed = moveSpeed;
+            }
+
+            if (Input.GetButtonDown("Jump") && Input.GetAxisRaw("Vertical") >= 0f)
+            {
+                jump = true;
+            }
+
+        }
+        
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
--- Project_LinsTale_Normal/Assets/Scripts/playerMovement_P2.cs
+++ Project_LinsTale_Normal/Assets/Scripts/playerMovement_P2.cs
@@ -7,19 +7,44 @@ public class playerMovement_P2 : MonoBehaviour
     public characterController2D controller;
     public Animator anim;
 
+    public bool ctrlable = true;
     public float moveSpeed = 40f;
 
     float horizontalMove = 0f;
+    float _moveSpeed;
+    bool jump = false;
+    private void Start()
+    {
+        _moveSpeed = moveSpeed;
 
+    }
     private void Update()
     {
-        //Debug.Log(Input.GetAxisRaw("Horizontal"));
-        horizontalMove = Input.GetAxisRaw("Horizontal2") * moveSpeed;
-        anim.SetFloat("moveSpeed", Mathf.Abs(horizontalMove));
+        if (ctrlable)
+        {
+            //Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+            horizontalMove = Input.GetAxisRaw("Horizontal2") * _moveSpeed;
+            anim.SetFloat("moveSpeed", Mathf.Abs(horizontalMove));
+            if (Input.GetAxisRaw("Vertical2") < 0f)
+            {
+                anim.SetBool("isCrouch", true);
+                _moveSpeed = moveSpeed / 2;
+            }
+            else
+            {
+                anim.SetBool("isCrouch", false);
+                _moveSpeed = moveSpeed;
+            }
+
+            
+
+        }
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
