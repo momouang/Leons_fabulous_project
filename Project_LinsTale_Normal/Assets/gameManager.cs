2775baa2cb4718bbe6e641730e088f8f3d413b3e
Leon <bsexp357079@gmail.com>
2022-01-19 00:04:42 +0800

    Stage 1 completed

--- Project_LinsTale_Normal/Assets/gameManager.cs
+++ Project_LinsTale_Normal/Assets/gameManager.cs
@@ -8,10 +8,24 @@ public class gameManager : MonoBehaviour
 
     [SerializeField]
     Animator panelAnimator;
-
+    int currentSceneIndex;
+    public int nextSceneIndex = 0;
+    public bool leonReady = false, lilyReady = false;
     public void Awake()
     {
         panelAnimator.Play("CameraCap_fadeOut");
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void Update()
+    {
+        if (leonReady && lilyReady)
+            StartCoroutine(loadNextSceneIE(nextSceneIndex));
+    }
+
+    public void gameoverReloadScene()
+    {
+        StartCoroutine(gameOverLoadScene(currentSceneIndex));
     }
 
     public void loadSceneIndex(int i)
@@ -23,6 +37,15 @@ public class gameManager : MonoBehaviour
     {
         yield return new WaitForSeconds(4);
         panelAnimator.Play("CameraCap_fadeBlack");
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(i);
+    }
+
+    IEnumerator loadNextSceneIE(int i)
+    {
+        yield return new WaitForSeconds(2);
+        panelAnimator.Play("CameraCap_fadeBlack");
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(i);
     }
 }
--- Project_LinsTale_Normal/Assets/mom_roomScript.cs
+++ Project_LinsTale_Normal/Assets/mom_roomScript.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class momScript : MonoBehaviour
+public class mom_roomScript : MonoBehaviour
 {
-    //public Animator leonAnim;
+    public Animator anim, leonAnim;
     public UnityEvent gmEvent;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
+        {
+            anim.SetBool("isStare", true);
             if (collision.GetComponent<playerState>().isVisible)
             {
                 collision.GetComponent<playerMovement>().ctrlable = false;
                 collision.GetComponent<playerMovement>().setMovementZero();
-                collision.GetComponent<playerState>().anim.Play("leon_scared");
+                leonAnim.Play("leon_scared");
                 gmEvent.Invoke();
             }
-                
+        }
+            
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            anim.SetBool("isStare", false);
     }
 }
